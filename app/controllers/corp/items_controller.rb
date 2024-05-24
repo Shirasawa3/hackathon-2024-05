311233a5ae218dae5a077450fd7e358aaedb1040
Funny_Silkie <ban.grumble99rar@gmail.com>
2024-05-25 02:09:30 +0900

    fix: lint

--- app/controllers/corp/items_controller.rb
+++ app/controllers/corp/items_controller.rb
@@ -5,7 +5,7 @@ class Corp::ItemsController < ApplicationController
     @items = ItemBasicInfo.eager_load(:item_type, :lent_histories)
                           .order(created_at: :asc)
     @items = @items.where('item_basic_infos.name LIKE ?', '%' + params[:s] + '%') if params[:s].present?
-    @items = @items.where(item_type_id: params[:type].to_i) if !params[:type].blank? && params[:type].to_i > 0
+    @items = @items.where(item_type_id: params[:type].to_i) if params[:type].present? && params[:type].to_i.positive?
     @items = @items.page(params[:page])
   end
 
