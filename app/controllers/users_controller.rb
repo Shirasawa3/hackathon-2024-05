8b61501bcea6e0a782f9ea355196172039684073
Shirasawa3 <yrry.nrm@gmail.com>
2024-05-26 14:38:07 +0900

    add: ユーザーページに絞り込み実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@
 class UsersController < UserControllerBase
   def show
     @lent_histories = @user.lent_histories.order(returned_at: :desc)
-    @lent_histories = @lent_histories.in_lent if params[:type].present? && params[:type].to_i == 1
-    @lent_histories = @lent_histories.not_in_lent if params[:type].present? && params[:type].to_i == 2
+    @lent_histories = @lent_histories.in_lent if params[:lent_status].present? && params[:lent_status].to_i == 1
+    @lent_histories = @lent_histories.not_in_lent if params[:lent_status].present? && params[:lent_status].to_i == 2
     @lent_histories = @lent_histories.page(params[:page])
   end
 end
